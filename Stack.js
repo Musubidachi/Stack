4d93b0c2b6cd5c03224d811ebe7414992fc102c1
Fabrizio Mangoni <fabriziom19@gmail.com>
2016-09-06 15:30:53 -0400

    Fixed a bunch of code

--- Stack.js
+++ Stack.js
@@ -2,50 +2,59 @@ var prompt = require('prompt-sync')();
 var arr = [];
 var playDecision = true;
 //runs the program until the user decides to stop playing
-while(playDecision){
+while (playDecision) {
     //gets and validates the user input
     var userSelection = validate();
+    userSelection = Number.parseInt(userSelection);
     //executes the corresponding command based on what they inputted
-    if(userSelection === 1){
-        arr.push
-    }else if(userSelection === 2){
-        console.log("current last value in stack " + arr[arr.length-1]);
-    }else{
-        if(isNaN(arr[arr.length-1])){
-            console.log('No value to pop! Try adding a value first!');
+    if (userSelection === 1) {
+        arr.push(getValueToPush());
+    } else if (userSelection === 2) {
+        if(arr.length == 0){
+            //no value
+            console.log("Nothing to show!");
         }else{
-            console.log("removing " + arr[arr.length-1]);
+            console.log("current last value in stack " + arr[arr.length - 1]);
+        }
+    } else {
+        if (arr.length == 0) {
+            console.log('No value to pop! Try adding a value first!');
+        } else {
+            console.log("removing " + arr[arr.length - 1]);
             arr.pop();
         }
     }
     //Determines if the user wants to continue playing.
     playDecision = getPlayDecision();
 }
+function getValueToPush(){
+    return prompt('Value?');
+}
 //logic for playing
-function getPlayDecision(){
-    while(true){
-        var userValue = prompt('Continue?');
-        
-        if(!isNaN(userValue) && userValue.toUpperCase==='YES' && userValue.toUpperCase === 'NO'){
+function getPlayDecision() {
+    while (true) {
+        userValue = prompt('Continue?');
+        //toUpper needed () - fix
+        if ('YES' == userValue.toUpperCase() || 'NO' == userValue.toUpperCase()) {
             break;
         }
         console.log('Please enter a valid value (YES or NO)');
     }
-    if(userValue === 'YES'){
+    if (userValue.toUpperCase() == 'YES') {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
 //logic for getting and validating a user input
-function validate(){
+function validate() {
 
-    while(true){
-    
+    while (true) {
+
         var userValue = prompt('Add (1) , View(2), Remove (3)');
-        
-        if(!isNaN(userValue) && userValue>=1 && userValue <=3){
+
+        if (!isNaN(userValue) && userValue >= 1 && userValue <= 3) {
             break;
         }
         console.log('Please enter a valid value');
